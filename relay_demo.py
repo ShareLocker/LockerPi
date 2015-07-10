b1bfa2710fdfeff2b1287590dd163beeabc0b29d
root <root@raspberrypi.(none)>
2015-07-10 15:27:52 -0400

    test script opens all lockers

--- relay_demo.py
+++ relay_demo.py
@@ -11,21 +11,36 @@ COL1 = 3
 rows = [ROW1, ROW2] #...
 cols = [COL1]
 
+rowl = [6, 13, 19, 26]
+coll = [16, 20]
+
 RPi.GPIO.setmode(RPi.GPIO.BCM)  # Set pin numbering to GPIO, not BOARD pin
-RPi.GPIO.setup(ROW1, RPi.GPIO.OUT) # Configure GPIO 2 as output
-RPi.GPIO.setup(ROW2, RPi.GPIO.OUT)
-RPi.GPIO.setup(COL1, RPi.GPIO.OUT)
+#RPi.GPIO.setup(ROW1, RPi.GPIO.OUT) # Configure GPIO 2 as output
+#RPi.GPIO.setup(ROW2, RPi.GPIO.OUT)
+#RPi.GPIO.setup(COL1, RPi.GPIO.OUT)
+
+for r in range(4):
+    RPi.GPIO.setup(rowl[r], RPi.GPIO.OUT)
+    
+for c in range(2):
+    RPi.GPIO.setup(coll[c], RPi.GPIO.OUT)
 
 def open_location(row, column):
-    RPi.GPIO.output(row, False)
-    RPi.GPIO.output(column, False)
+    row_GPIO = rowl[row]
+    col_GPIO = coll[column]
+    RPi.GPIO.output(row_GPIO, False)
+    RPi.GPIO.output(col_GPIO, False)
     time.sleep(3)
-    RPi.GPIO.output(row, True)
-    RPi.GPIO.output(column, True)
+    RPi.GPIO.output(row_GPIO, True)
+    RPi.GPIO.output(col_GPIO, True)
 
 while True:
-    for row in rows:
-        for col in cols:
-            open_location(ROW1, COL1)
-            time.sleep(2)
+    for r in range(4):
+        for c in range(2):
+            open_location(r, c)
+            time.sleep(0.5)
+#    for row in rows:
+#        for col in cols:
+#            open_location(ROW1, COL1)
+#            time.sleep(2)
 RPi.GPIO.cleanup()
