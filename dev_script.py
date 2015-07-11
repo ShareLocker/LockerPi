de4aeeb460fccb542839ac916d6a1a10d114322c
Alan R <alan.rominger@gmail.com>
2015-07-10 23:42:02 -0400

    cleaned up and made pythonic using real computer

---
+++ dev_script.py
@@ -0,0 +1,60 @@
+import RPi.GPIO
+import time
+import os
+import datetime
+import requests
+
+# hardware configuration
+Nrow = 4
+Ncol = 2
+secret = "2"
+host_ip = "https://sharelockers.localtunnel.me"
+connect = host_ip + '/hubs/connected/' + secret
+poll = host_ip + '/hubs/poll/' + secret
+finished = host_ip + '/hubs/finished/' + secret
+rowl = [6, 13, 19, 26]
+coll = [16, 20]
+
+# behavior configuration
+open_time = 3 # open the latch for ... seconds
+poll_time = 2
+
+    # begin setup code
+def PiPinSetup(initial=False):
+    RPi.GPIO.setmode(RPi.GPIO.BCM)  # Set pin numbering to GPIO, not BOARD pin
+	for pin in rowl + coll:
+        RPi.GPIO.setup(pin, RPi.GPIO.OUT)
+		if initial:
+			RPi.GPIO.output(pin, True)
+
+
+PiPinSetup(initial=True)
+RPi.GPIO.cleanup()
+resp = requests.get(connect)
+
+# functions for continuous polling operation
+def poll_status():
+    resp = requests.get(poll)
+    action, col, row = tuple(char for char in resp.text)[:3]
+    return action, col, row
+
+
+def open_location(column, row):
+    row_GPIO = rowl[row]
+    col_GPIO = coll[column]
+    RPi.GPIO.output(row_GPIO, False)
+    RPi.GPIO.output(col_GPIO, False)
+    time.sleep(open_time)
+    RPi.GPIO.output(row_GPIO, True)
+    RPi.GPIO.output(col_GPIO, True)
+
+# constant loop
+while True:
+    action, col, row = poll_status()
+    if action == "?":
+        PiPinSetup()
+        open_location(col, row)
+        RPi.GPIO.cleanup()
+        time.sleep(open_time)
+    else:
+        time.sleep(poll_time)
--- relay_demo.py
+++ relay_demo.py
@@ -7,17 +7,11 @@ import requests
 # hardware configuration
 Nrow = 4
 Ncol = 2
-secret = "2"
-host_ip = "https://sharelockers.localtunnel.me"
-connect = host_ip + '/hubs/connected/' + secret
-poll = host_ip + '/hubs/poll/' + secret
-finished = host_ip + '/hubs/finished/' + secret
 rowl = [6, 13, 19, 26]
 coll = [16, 20]
 
 # behavior configuration
 open_time = 3 # open the latch for ... seconds
-poll_time = 2
 
     # begin setup code
 def PiPinSetup():
@@ -27,44 +21,22 @@ def PiPinSetup():
     for c in range(2): # configure the column output pins
         RPi.GPIO.setup(coll[c], RPi.GPIO.OUT)
 
-resp = requests.get(connect)
-
-# functions for continuous polling operation
-def poll_status():
-    resp = requests.get(poll)
-    action, col, row = tuple(char for char in resp.text)[:3]
-    return action, col, row
-
 
 def open_location(column, row):
     row_GPIO = rowl[row]
     col_GPIO = coll[column]
     RPi.GPIO.output(row_GPIO, False)
     RPi.GPIO.output(col_GPIO, False)
-    time.sleep(3)
+    time.sleep(open_time)
     RPi.GPIO.output(row_GPIO, True)
     RPi.GPIO.output(col_GPIO, True)
 
+PiPinSetup()
 
+for k in range(2): # cycle 2 times
+   for r in range(Nrow):
+       for c in range(Ncol):
+           open_location(c, r)
+           time.sleep(0.5)
 
-#while True:
-#for k in range(1):
-#    for r in range(4):
-#        for c in range(2):
-#            open_location(r, c)
-#            time.sleep(0.5)
-#    for row in rows:
-#        for col in cols:
-#            open_location(ROW1, COL1)
-#            time.sleep(2)
-
-# constant loop
-while True:
-    action, col, row = poll_status()
-    if action == "?":
-        PiPinSetup()
-        open_location(col, row)
-        RPi.GPIO.cleanup()
-        time.sleep(open_time)
-    else:
-        time.sleep(poll_time)
+RPi.GPIO.cleanup()
